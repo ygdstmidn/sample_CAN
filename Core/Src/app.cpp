06d35e4d291c48666e1aa7b61bc8bdbe933dec6f
ygdstmidn <2022d12@stu.gifu-nct.ac.jp>
2026-07-12 22:54:01 +0900

    ✅ マイコン生存確認

--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -1,6 +1,14 @@
 // main code here!!
 
 #include <main.h>
+#include <usart.h>
+#include <ukaikokko/ukaikokko.h>
+#include <stdio.h>
+
+ukaikokko::InterruptBufferedUART<256, 1024, 1024> pc(&huart2);
+ukaikokko::GPOutput led(DebugLED_GPIO_Port, DebugLED_Pin);
+
+static bool setupDone = false;
 
 #ifdef __cplusplus
 extern "C"
@@ -9,6 +17,15 @@ extern "C"
 
     void user_setup(void)
     {
+        led.write(1);
+        printf("sample_CAN\r\n");
+        printf(__DATE__ "\r\n");
+        printf(__TIME__ "\r\n");
+
+        pc.begin();
+
+        led.write(0);
+        setupDone = true;
     }
 
     void user_loop(void)
@@ -18,8 +35,50 @@ extern "C"
 
         if (now - pre >= 10)
         {
+            static int count = 0;
+            count++;
+            if (count >= 100)
+            {
+                led.toggle();
+                printf(">now:%lu\r\n", now);
+                count = 0;
+            }
+
+            while (pc.available() > 0)
+            {
+                uint8_t data = static_cast<uint8_t>(pc.read());
+                putchar(data);
+                fflush(NULL);
+            }
+
+            pc.periodic();
+
             pre = now;
         }
+    }
+
+    int _write(int file, char* ptr, int len)
+    {
+        if (!setupDone)
+        {
+            HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, 10); // ブロッキング
+            return len;
+        }
+        else
+        {
+            pc.write((uint8_t*)ptr, len);
+            return len;
+        }
+    }
+
+    void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart)
+    {
+        pc.RxCplt(huart);
+    }
+
+    void HAL_UART_TxCpltCallback(UART_HandleTypeDef* huart)
+    {
+        pc.TxCplt(huart);
     }
 
 #ifdef __cplusplus
